20459bfbac88bba3954338f1939565f9ca9a2446
Saptarshi <roni.bagchi@gmail.com>
2021-05-10 00:04:53 +0530

    form validation is done

---
+++ components/Form.js
@@ -0,0 +1,72 @@
+import { AnimatePresence, motion } from "framer-motion";
+export default function Form({
+  onSubmit,
+  errors,
+  setErrors,
+  fields,
+  setFields,
+}) {
+  console.group("errors");
+  console.log(errors);
+  console.groupEnd();
+  return (
+    <div className="lg:w-2/5 sm:w-3/5 h-auto px-10 py-5 mx-auto mt-32 bg-white rounded-md shadow-md">
+      <h3 className="text-center font-semibold text-xl ">Login to continue</h3>
+      <form onSubmit={onSubmit} className="my-3">
+        <div>
+          <label>Username</label>
+        </div>
+        <input
+          value={fields.username || ""}
+          type="text"
+          name="username"
+          onChange={(e) => setFields({ ...fields, username: e.target.value })}
+          className="w-full px-1 py-2 mt-2 border-gray-400 border rounded-md"
+          placeholder="Enter your username"
+        ></input>
+        {errors.username && (
+          <AnimatePresence>
+            <motion.p
+              initial={{ y: -50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              className="text-red-500 text-sm text-semibold"
+            >
+              {errors.username}{" "}
+            </motion.p>
+          </AnimatePresence>
+        )}
+
+        <div className="mt-4">
+          <label>Password</label>
+        </div>
+        <input
+          value={fields.password || ""}
+          onChange={(e) => setFields({ ...fields, password: e.target.value })}
+          name="password"
+          type="password"
+          className="w-full px-1 py-2 mt-2 border-gray-400 border rounded-md"
+          placeholder="Enter your password"
+        ></input>
+        {errors.password && (
+          <AnimatePresence>
+            <motion.p
+              initial={{ y: -50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: 50, opacity: 0 }}
+              className="text-red-500 text-sm text-semibold"
+            >
+              {errors.password}{" "}
+            </motion.p>
+          </AnimatePresence>
+        )}
+
+        <button
+          type="submit"
+          className="bg-blue-700 w-full px-4 py-2 my-4 rounded-md text-white"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,49 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import Form from "../components/Form";
 
 export default function Home() {
+  const router = useRouter();
+  const [fields, setFields] = useState({
+    username: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let generatedErrors = {};
+    //console.log(`${fields.username.length} and ${fields.password.length}`);
+    if (fields.username.length === 0) {
+      console.log("username is blank actually");
+      generatedErrors.username = "Username cannot be empty";
+    }
+
+    if (fields.password.length === 0)
+      generatedErrors.password = "Password cannot be empty";
+    if (generatedErrors === {}) {
+      setErrors(generatedErrors);
+      return;
+    }
+
+    router.replace("/about");
+  };
+
   return (
     <div className="w-full h-screen">
-      <div className="w-2/5 h-auto px-10 py-5 mx-auto mt-32 bg-white rounded-md shadow-md">
-        <h3 className="text-center font-semibold text-xl ">
-          Login to continue
-        </h3>
-        <form onSubmit={null}>
-          <div>
-            <label>Username</label>
-          </div>
-          <input
-            type="text"
-            className="w-full px-4 py-2"
-            placeholder="Enter your username"
-          ></input>
-
-          <div className="mt-4">
-            <label>Password</label>
-          </div>
-          <input
-            type="password"
-            className="w-full px-4 py-2"
-            placeholder="Enter your password"
-          ></input>
-        </form>
-      </div>
+      <Form
+        onSubmit={handleSubmit}
+        errors={errors}
+        setErrors={setErrors}
+        fields={fields}
+        setFields={setFields}
+      />
     </div>
   );
 }
