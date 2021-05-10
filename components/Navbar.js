334f652b6c40d0eab2275450d926e87525447e06
Saptarshi <roni.bagchi@gmail.com>
2021-05-11 01:27:53 +0530

    overflow done

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,9 +9,17 @@ export default function Navbar() {
       initial={{ y: -100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       exit={{ y: -100, opacity: 0 }}
-      className="h-auto z-10 px-8 py-5 bg-blue-600 w-full"
+      className="h-auto z-10 px-8 py-5 bg-blue-600 w-full flex justify-between"
     >
-      <p className="text-white">{route}</p>
+      <p className="text-white text-2xl">NextApp</p>
+      <div className="lg:flex align-middle">
+        <div className="mx-2 text-white border-white border-solid border-2 px-4 py-2 rounded-md">
+          About
+        </div>
+        <div className="mx-2 text-white border-white border-solid border-2 px-4 py-2 rounded-md">
+          Pricing
+        </div>
+      </div>
     </motion.div>
   );
 }
--- pages/about.js
+++ pages/about.js
@@ -6,13 +6,18 @@ export default function about() {
       initial={{ x: "100%", opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
       exit={{ x: "-100%", opacity: 0 }}
-      className="flex"
+      className="flex h-screen"
     >
-      <div className="w-1/5 px-4 py-2 bg-white h-screen overflow-y-auto">
+      <div className="w-1/5 px-4 py-2 bg-white overflow-y-auto">
         {<LoremIpsum p={10} />}
       </div>
-      <div className="w-4/5 p-5 bg-yellow-500 overflow-y-scroll h-screen">
-        {<LoremIpsum p={50} />}
+      <div className="w-4/5 bg-yellow-500 overflow-y-hidden flex h-screen">
+        <div className="w-3/5 p-5 overflow-y-auto h-screen">
+          {<LoremIpsum p={50} />}
+        </div>
+        <div className="w-2/5 p-5 bg-blue-300 overflow-y-auto  h-screen">
+          {<LoremIpsum p={10} />}
+        </div>
       </div>
     </motion.div>
   );
